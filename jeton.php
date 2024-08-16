a1bb5f404bfa8ea5e5b7e1165eebf83ca10b196d
Guy Verghote <guy.verghote@saint-remi.net.fr>
2024-08-16 11:52:50 +0200

    Le 16/08/2024 (Guy Verghote) : Mise à jour classe Jeton

--- jeton.php
+++ jeton.php
@@ -29,15 +29,15 @@ class Jeton {
     public static function verifier() {
         // Vérifiez la correspondance des jetons
         if (!isset($_SESSION['token'])) {
-            Erreur::envoyerReponse("Le jeton de vérification n'est pas présent sur le serveur.", 'system');
+            Erreur::envoyerReponse("Le jeton de vérification n'est pas présent sur le serveur.", 'global');
             return false;
         }
         if (!isset($_REQUEST['token'])) {
-            Erreur::envoyerReponse("Le jeton de vérification n'a pas été transmis.", 'system');
+            Erreur::envoyerReponse("Le jeton de vérification n'a pas été transmis.", 'global');
             return false;
         }
         if ($_SESSION['token'] !== $_REQUEST['token']) {
-            Erreur::envoyerReponse("Absence d'un jeton valide vous autorisant à réaliser cette opération.", 'system');
+            Erreur::envoyerReponse("Absence d'un jeton valide vous autorisant à réaliser cette opération.", 'global');
             return false;
         }
         return true;
