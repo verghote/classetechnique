085c2a71b14bfb4beaf5e5ccf1724936fcec24cc
Guy Verghote <guy.verghote@saint-remi.net.fr>
2024-08-23 09:25:10 +0200

    Le 23/08/2024 (Guy Verghote) : Mise à jour de la classe TraficIP

--- traficip.php
+++ traficip.php
@@ -5,21 +5,25 @@ declare(strict_types=1);
  * Classe TraficIp : Classe permettant de mémoriser le trafic afin de détecter les ip à bloquer
  *
  * @Author : Guy Verghote
- * @Date :18/08/2024
+ * @Date :23/08/2024
  */
 class TraficIp
 {
     /**
-     * Enregistre l'ip du visiteur à l'origine de la requête
+     * Supprime le trafic de la semaine précédente et enregistre l'ip du visiteur à l'origine de la requête
      * @return bool
      */
     public static function ajouter(string $ip)
     {
         try {
+            $db = Database::getInstance();
+            $sql = <<<EOD
+            DELETE FROM traficip WHERE horodatage < NOW() - INTERVAL 1 week;  
+EOD;
+            $db->exec($sql);
             $sql = <<<EOD
             INSERT INTO traficip(ip) VALUES (:ip);
 EOD;
-            $db = Database::getInstance();
             $curseur = $db->prepare($sql);
             $curseur->bindParam('ip', $ip);
             $curseur->execute();
